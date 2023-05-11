c96fdb76756753fd4266534333fe51bebe986be1
Sandy <peacesandy04@gmail.com>
2023-05-11 07:28:05 -0700

    Styled nav and footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import {FaGithub, FaLinkedin, FaTwitter, } from 'react-icons/fa'
 import '../Styles/Footer.css'
 
 function Footer() {
+        
+       function handleLinkClick(event, link) {
+          event.preventdefault()
+          window.open(link, '_blank')
+       }
+
   return (
     <div className='footer'>
+      <div>
       <p>Would you like to support my content</p>
       <button>Buy me a coffee</button>
+      </div>
+      <div>
+        <ul>
+          <li> <a href="http://twitter.com/PeaceSandy3" onClick={(event) => handleLinkClick(event, 'http://twitter.com/PeaceSandy3' )}> <FaTwitter/> </a> </li>
+          <li> <a href="https://www.linkedin.com/in/peace-sandy-bb7a691b0" onClick={(event) => handleLinkClick(event, 'https://www.linkedin.com/in/peace-sandy-bb7a691b0')}> <FaLinkedin/></a> </li>
+          <li> <a href="https://github.com/Peacesandy" onClick={(event) => handleLinkClick(event, 'https://github.com/Peacesandy')}> <FaGithub/></a> </li>
+        </ul>
+      </div>
     </div>
   )
 }
