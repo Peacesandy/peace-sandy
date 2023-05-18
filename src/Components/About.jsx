20f0dc33a5462fb518deeb003eaee43a0eac8660
Sandy <peacesandy04@gmail.com>
2023-05-18 06:15:33 -0700

    changed style

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,58 +1,25 @@
 import React from 'react'
-import Emoji from '../Images/waving-hand_1f44b.gif'
-import Profile from '../Images/image2 (2).jpeg'
 import '../Styles/About.css'
-import { motion } from "framer-motion";
+
 
 function About() {
 
   return (
     <> 
     <div className='about-container' id='about'>
-        <h1 className='intro-section'>Hi There ! <img src={Emoji} alt='Emoji' className='emoji'/></h1>
-        <br/>
-        <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    > 
-         <h1>My name is Peace Sandy and I'm a 
-         <br/> front-end developer based in Lagos, Nigeria.  
-         <br/> I am passionate about building interactive 
-         <br/>  experiences on the web with a focus on 
-         <br/> animations and interactions.</h1>
-         </motion.div>
-        <figure className='card'>
-        <img src={Profile} alt='My Profile' className='profile'/>
-        </figure>
-        <h1>
-         I am also a designer skilled in digital design,
-         art direction and motion for digital experiences.
-         I design unique web experiences for forward-thinking 
-         brands and individuals.
-        </h1>
-        <h1>
-        I am skilled in the following technologies and disciplines.
-        </h1>
-        <div className='skill'>
-          <p>Skills</p>
-          <p>I enjoy diving into and learning new things. Here's a list of technologies I've worked with</p>
-          <div className=''>
-            <p>HTML</p>
-          </div>
-          <div className=''>
-            <p>CSS</p>
-          </div>
-          <div>
-            <p>Javascript</p>
-          </div>
-          <div>
-            <p>React</p>
-          </div>
-          <div>
-            <p>Git</p>
-          </div>
-        </div>
+         <h1 className='About-intro'>About</h1>
+         <div>
+          <p className='About-text1'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+           <br/> Repellendus fugiat aperiam minus ullam adipisci ratione, qu 
+           <br/> ae laborum quam, perferendis deserunt quos officiis, consequ
+           <br/>  untur molestiae eveniet. deserunt quos officiis, consequ</p>
+         </div>
+         <div>
+         <p className='About-text2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+           <br/> Repellendus fugiat aperiam minus ullam adipisci ratione, qu 
+           <br/> ae laborum quam, perferendis deserunt quos officiis, consequ
+           <br/>  untur molestiae eveniet. deserunt quos officiis, consequ</p>
+         </div>
     </div>
     </>
   )
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import image from '../Images/image2 (2).jpeg'
 import '../Styles/Home.css'
 
 function Home() {
@@ -7,19 +6,14 @@ function Home() {
     <div className='container' id='home'>
 
       <div className='image-container'>  
-      <img src={image} alt='author' className='image'/>
       </div>
 
       <div className='text'>
-      <h1 className='name'>Hello, I'm Peace Sandy</h1>
+      <h1>Peace Sandy</h1>
       <br/>
-        <p className='bio'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-         <br/> Incidunt perferendis, molestias optio laborum excepturi soluta 
-         <br/> quas magni itaque recusandae. Perferendis aliquid sed mollitia enim possimus!</p>
+        <h1 className='bio'> Frontend Software Engineer & Technical Writer. </h1>
         </div>
-          <div>
-            <button>Download My Resume</button>
-          </div>
+        <hr/>
     </div>
   )
 }
