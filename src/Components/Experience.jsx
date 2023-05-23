f437fc86fadd08c77ec38f79cae4481c8bfaae9f
Sandy <peacesandy04@gmail.com>
2023-05-23 10:31:38 -0700

    styled certification page

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import '../Styles/Experience.css'
 
+import Html from '../Images/html.skills.png'
+
 function Experience() {
   return (
     <div id='experience'>
       <hr/>
+      <div className='certification-container'>   
+    <h1 class="certification-heading">Certicication and Experience</h1>
+    </div>
+
       <div class="timeline">
-    <h1 class="heading">Certicication and Experience</h1>
     <div class="card">
         <div class="card-body">
+        <img src={Html} alt="A git card" className='html'/>
             <h1 class="card-title">2000-2002</h1>
             <p class="card-detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sequi recusandae laborum ipsam dignissimos nostrum vitae provident officia, consectetur ab accusantium corrupti exercitationem temporibus repellat non magni cupiditate ea reprehenderit.</p>
         </div>
