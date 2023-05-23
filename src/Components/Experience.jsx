8a1e93017a5301da3233dd516105fcb04ad7121b
Sandy <peacesandy04@gmail.com>
2023-05-23 16:25:35 -0700

    Added more custom images

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import '../Styles/Experience.css'
 
-import Html from '../Images/html.skills.png'
+import HNG from '../Images/HNG.jpg'
+import stutern from '../Images/stutern.jpg'
+import zuri from '../Images/zuri.jpg'
+import Educative from '../Images/educative.png'
+import Collab from '../Images/collablab.png'
 
 function Experience() {
   return (
     <div id='experience'>
       <hr/>
       <div className='certification-container'>   
-    <h1 class="certification-heading">Certicication and Experience</h1>
+    <h1 class="certification-heading">Certification and Experience</h1>
     </div>
 
       <div class="timeline">
     <div class="card">
         <div class="card-body">
-        <img src={Html} alt="A git card" className='html'/>
+        <img src={stutern} alt="A git card" className='html'/>
             <h1 class="card-title">2000-2002</h1>
             <p class="card-detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sequi recusandae laborum ipsam dignissimos nostrum vitae provident officia, consectetur ab accusantium corrupti exercitationem temporibus repellat non magni cupiditate ea reprehenderit.</p>
         </div>
@@ -22,6 +26,7 @@ function Experience() {
         
       <div class="card">
         <div class="card-body">
+        <img src={HNG} alt="A git card" className='html'/>
             <h1 class="card-title">2000-2002</h1>
             <p class="card-detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sequi recusandae laborum ipsam dignissimos nostrum vitae provident officia, consectetur ab accusantium corrupti exercitationem temporibus repellat non magni cupiditate ea reprehenderit.</p>
         </div>
@@ -29,6 +34,7 @@ function Experience() {
 
       <div class="card">
         <div class="card-body">
+        <img src={Educative} alt="A git card" className='html'/>
             <h1 class="card-title">2000-2002</h1>
             <p class="card-detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sequi recusandae laborum ipsam dignissimos nostrum vitae provident officia, consectetur ab accusantium corrupti exercitationem temporibus repellat non magni cupiditate ea reprehenderit.</p>
         </div>
@@ -36,6 +42,7 @@ function Experience() {
 
       <div class="card">
         <div class="card-body">
+        <img src={Collab} alt="A git card" className='html'/>
             <h1 class="card-title">2000-2002</h1>
             <p class="card-detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sequi recusandae laborum ipsam dignissimos nostrum vitae provident officia, consectetur ab accusantium corrupti exercitationem temporibus repellat non magni cupiditate ea reprehenderit.</p>
         </div>
@@ -43,6 +50,7 @@ function Experience() {
 
       <div class="card">
         <div class="card-body">
+        <img src={zuri} alt="A git card" className='html'/>
             <h1 class="card-title">2000-2002</h1>
             <p class="card-detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sequi recusandae laborum ipsam dignissimos nostrum vitae provident officia, consectetur ab accusantium corrupti exercitationem temporibus repellat non magni cupiditate ea reprehenderit.</p>
         </div>
