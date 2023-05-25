7f1364a1b7a8a6b6c5402a271b704180b7d27f7f
Sandy <peacesandy04@gmail.com>
2023-05-25 07:15:13 -0700

    finished publication page

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import  '../Styles/Blog.css'
-import Git from '../Images/github-skills.png'
+
+import firstarticle from '../Images/firstarticle.png'
+import secondarticle from '../Images/secondarticle.png'
+import thirdarticle from '../Images/fiftharticle.png'
+
 
 function Blog() {
   return (
     <div id='blog'> 
+    <hr/>
+    <div>
+      <h1>Publications</h1>
+    </div>
+    <div className='projects-container'>
+       
+    <div className='project-container project-card'>
+    <img src={firstarticle} alt="A git card" className='article'/>
+        <h3 className='project-title'>Array</h3>
+        <p className='project-details'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis corrupti beatae voluptatibus!</p>
+        <a href="#" target='_blank' className='project-link'>Check It Out</a>
+    </div>
+
+    <div className='project-container project-card'>
+    <img src={secondarticle} alt="A git card" className='article'/>
+        <h3 className='project-title'>Array</h3>
+        <p className='project-details'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis corrupti beatae voluptatibus!</p>
+        <a href="#" target='_blank' className='project-link'>Check It Out</a>
+    </div>
+
+    <div className='project-container project-card'>
+    <img src={thirdarticle} alt="A git card" className='article'/>
+        <h3 className='project-title'>Array</h3>
+        <p className='project-details'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis corrupti beatae voluptatibus!</p>
+        <a href="#" target='_blank' className='project-link'>Check It Out</a>
+    </div>
+
+    </div>
     </div>
       
   )
