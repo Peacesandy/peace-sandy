404fee4ed3e09c32847883808e6515a6eab1a4cd
Sandy <peacesandy04@gmail.com>
2023-05-25 08:03:18 -0700

    finished styling contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -9,24 +9,34 @@ function Contact() {
         <br/>
         <p>Invite me to speak at your conference, tell me about other</p>
       </div>
+      <div className='contact-form-container'> 
+      <div className='contact-form'>  
       <form>
+        <div className='form-control'> 
         <label htmlFor='name'>Name</label>
         <input
           id='name'
           type='text'
         />
+        </div>
+        <div className='form-control'> 
         <label htmlFor='email'>Email</label>
         <input
           id='email'
           type='text'
         />
+        </div>
+        <div className='form-control'> 
         <label htmlFor='message'>Message</label>
         <textarea
           id='message'
           type='text'
         ></textarea>
-        <button type='submit'>send</button>
+        </div>
+        <button type='submit' className='submit-btn'>send</button>
       </form>
+      </div>
+      </div>
       <hr/>
     </div>
   )
