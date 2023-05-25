68571e2e8a37211609eb72b2dda621cb1b3cee83
Sandy <peacesandy04@gmail.com>
2023-05-25 15:17:01 -0700

    started media queries

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,27 +1,12 @@
 import React from 'react'
-import {FaGithub, FaLinkedin, FaTwitter, } from 'react-icons/fa'
 import '../Styles/Footer.css'
 
 function Footer() {
-        
-       function handleLinkClick(event, link) {
-          event.preventdefault()
-          window.open(link, '_blank')
-       }
 
   return (
     <div className='footer' id='footer'>
-      <div>
-      <p>Would you like to support my content</p>
-      <button>Buy me a coffee</button>
-      </div>
-      <div>
-        <ul className='footer-ul'>
-          <li> <a href="http://twitter.com/PeaceSandy3" onClick={(event) => handleLinkClick(event, 'http://twitter.com/PeaceSandy3' )} className='footer-icon'> <FaTwitter/> </a> </li>
-          <li> <a href="https://www.linkedin.com/in/peace-sandy-bb7a691b0" onClick={(event) => handleLinkClick(event, 'https://www.linkedin.com/in/peace-sandy-bb7a691b0')} className='footer-icon'> <FaLinkedin/></a> </li>
-          <li> <a href="https://github.com/Peacesandy" onClick={(event) => handleLinkClick(event, 'https://github.com/Peacesandy')} className='footer-icon'> <FaGithub/></a> </li>
-        </ul>
-      </div>
+         <p class="copy">&copy; Copyright 2023</p>
+         <p className='copy'>Built with ❤️ by Peace Sandy</p>
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,35 +1,38 @@
 import React from 'react'
 import '../Styles/Navbar.css'
-import { FaHome, FaUserTie, FaRegFileCode,  FaAddressBook, FaWrench, FaRegEdit} from "react-icons/fa";
 
-
+import {FaUserTie} from 'react-icons/fa'
 
 function Navbar() {
   return (
     <nav className='sticky'>
+      <h1>Peace Sandy</h1>
       <ul className='nav-ul'>
         <li className='nav-li'>
-          <a href="#home">Home <FaHome size={15}/> </a>
+          <a href="#home">Home</a>
         </li>
         <li className='nav-li'>
-          <a href="#about">About <FaUserTie size={15}/> </a>
+          <a href="#about">About</a>
         </li>
         <li className='nav-li'>
-          <a href="#skills">Skills <FaRegFileCode size={15}/> </a>
+          <a href="#skills">Skills</a>
         </li>
         <li className='nav-li'>
-          <a href="#experience">Experience <FaAddressBook size={15}/> </a>
+          <a href="#experience">Experience</a>
         </li>
         <li className='nav-li'>
           <a href="#project">Project</a>
         </li>
         <li className='nav-li'>
-          <a href="#blog"> Blog <FaWrench size={15}/> </a>
+          <a href="#blog"> Blog </a>
         </li>
         <li className='nav-li'>
-          <a href="#contact"> Contact <FaRegEdit size={15}/> </a>
+          <a href="#contact"> Contact</a>
         </li>
       </ul>
+      <button className='burger-menu' id='burger-menu'>
+        <FaUserTie/>
+      </button>
     </nav>
   )
 }
