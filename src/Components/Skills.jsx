654c004e40227139be7836c83b37bd5e71a869d6
Sandy <peacesandy04@gmail.com>
2023-05-26 04:23:15 -0700

    still adding media queries

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -9,7 +9,7 @@ import Firestore from '../Images/Firestore.png'
 import Styled  from '../Images/redux.png'
 import Markdown from '../Images/markdown.png'
 import Git from '../Images/git.png'
-import Figma from '../Images/figma.png'
+import Figma from '../Images/figma3.png'
 import Sass from '../Images/icons8-sass.svg'
 
 
