298539c5da33ddde6fbc9b2b0bae99d3bc5f26be
Sandy <peacesandy04@gmail.com>
2023-05-26 10:33:26 -0700

    resized navbar

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import '../Styles/Contact.css'
 
-import {FaGithub, FaLinkedin, FaTwitter, } from 'react-icons/fa'
+
 
 function Contact() {
-  
-  function handleLinkClick(event, link) {
-    event.preventdefault()
-    window.open(link, '_blank')
- }
+
   return (
     <div className='contact-container' id='contact'>
       <div className='contact-text'>
@@ -44,12 +40,7 @@ function Contact() {
       </form>
       </div>
       </div>
-      <div className='socials'>
-      <a href="http://twitter.com/PeaceSandy3" onClick={(event) => handleLinkClick(event, 'http://twitter.com/PeaceSandy3' )} className='side-icon'> <FaTwitter size={30}/> </a> 
-           <a href="https://www.linkedin.com/in/peace-sandy-bb7a691b0" onClick={(event) => handleLinkClick(event, 'https://www.linkedin.com/in/peace-sandy-bb7a691b0')} className='side-icon'> <FaLinkedin size={30}/></a> 
-      <a href="https://github.com/Peacesandy" onClick={(event) => handleLinkClick(event, 'https://github.com/Peacesandy')} className='side-icon'> <FaGithub size={30}/></a> 
-        
-      </div>
+      
     </div>
   )
 }
